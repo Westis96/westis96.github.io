f928a41d2c18050be999cfd9cab5376de4bebc5d
Isak Westerlund <isak1096@gmail.com>
2018-01-26 17:31:55 +0200

    HIDPI scaling messing things up

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ var clearButton;
 var painting = false;
 var current;
 var previous;
-var img;
+//var img;
 var offset = 0;
 function centerButtons() {
   saveButton.position(windowWidth/2, windowHeight/2 + 96);
@@ -14,11 +14,11 @@ function setup() {
   createCanvas(128, 128);
   background(0);
   imageMode(CORNER);
-  img = createGraphics(128, 128);
-  img.background(250);
-  img.fill(0);
-  img.stroke(0);
-  img.strokeWeight(10);
+  //img = createGraphics(128, 128);
+  background(250);
+  fill(0);
+  stroke(0);
+  strokeWeight(10);
   current = createVector(0,0);
   previous = createVector(0,0);
   saveButton = createButton('Save Image');
@@ -29,13 +29,13 @@ function setup() {
 }
 
 function draw() {
-  image(img, offset, offset);
+  //image(img, offset, offset);
   current.x = mouseX - offset;
   current.y = mouseY - offset;
 
   if(painting) {
-    img.ellipse(mouseX - offset, mouseY - offset, 0, 0);
-    img.line(previous.x, previous.y, current.x, current.y);
+    ellipse(mouseX - offset, mouseY - offset, 0, 0);
+    line(previous.x, previous.y, current.x, current.y);
   }
 
   previous.x = current.x;
@@ -44,12 +44,12 @@ function draw() {
 }
 
 function changeBG() {
-  img.background(250);
+  background(250);
 }
 
 function saveImage() {
   saveCanvas(img, 'char', 'jpg');  
-  img.background(250);
+  background(250);
 }
 
 function windowResized() {
